4ec7dc2b6fd13ca388d582ed6479b8bc2e9a3451
Ren <36342803+Reptop@users.noreply.github.com>
2021-10-05 09:24:15 -0700

    Actaully made it such that test isn't a palindrome
    How did I even mess this up, im bad.

--- pal.cpp
+++ pal.cpp
@@ -1,20 +1,9 @@
 //Author: Raed K 
+//10/5/21 
+//This program basically checks if a word or sentence is a panidrome 
 #include <iostream> 
 #include <cstring>
 using namespace std; 
-
-//function that returns true or false based on comparing char array forwards and backwards 
-bool isPal(char x[80], int len) {
-     for(int i = 0; i < len; i++){
-        if(x[i] != x[len-i-1]) { //compares with the array running  backwards 
-            return false; 
-        }
-        else {
-            return true;    
-        }
-    }
-}
-
 
 // Driver code
 int main() {
@@ -22,11 +11,13 @@ int main() {
     //variables 
     char input[80];
     char input2[80];
+    char input3[80]; 
 
     cout << "Enter a word or sentence: "; 
     cin.getline(input, 80); 
     int len = strlen(input); 
-    int k = 0; 
+    int k = 0; // counters for each array
+    int l = 0; 
 
     for (int i = 0; i < len; i++) {
         if(isalpha(input[i])) { //checks if alphanumeric 
@@ -35,13 +26,24 @@ int main() {
                 //after one iteration, c increases by 1, and the next valid char in input1 gets inserted into input2 
         }
     }
-    len = k; //redeclare the new length of char array input2 after punctuation is removed 
+    len = k; //redeclare the new length of char array input2 after punctuation is removed
 
-    if(!isPal(input2, k)) {
-        cout << "Not a Palindrome" << endl;
-    }
-    else {
-        cout << "Its a Palindrome!" << endl; 
-    }    
+    for (int i = len - 1; i >= 0; i--) { //for loop backwards and insert reversed array into another array
+      input3[l] = input2[i]; 
+      l++; 
+  } 
+    //Display the different stages of the program 
+    cout << "Filtered Input: " << input2 << endl; 
+    cout << "Reversed Input: " << input3 << endl; 
+
+  //str compare the filtered input and reversed input
+  if (strcmp(input3, input2) == 0) { 
+      //if true, return as a palindrome 
+      cout << "Its a Palindrome" << endl; 
+  } 
+  else {
+    //otherwise its not a palindrome 
+    cout << "Not a palindrome"; 
+  }
+
 }
-
